8ff38e3abdeff7560acdbfbe0852ff6d44498b27
Stev <topshelfsmith@gmail.com>
2013-06-24 11:56:55 -0400

    First Commit
    Moving code to github, no changes made yet.

---
+++ phnxClose/plugin.js
@@ -0,0 +1,22 @@
+﻿// this plugin is custom, you must have jquery and phnx-Modals in place
+
+CKEDITOR.plugins.add( 'close', {
+    icons: 'close',
+    init: function( editor ) {
+        editor.addCommand( 'closeWindow', {
+        	exec: function( editor ) {
+        		var aaa = '#' + CKEDITOR.currentInstance.name;
+        		var bbb = $(aaa).closest('.modal-holder').attr('id');
+            	hideModal(bbb);
+            	CKEDITOR.currentInstance.setData('');
+        	}
+        });
+        
+        editor.ui.addButton( 'Close', {
+			label: 'Close Edit Window',
+			command: 'closeWindow',
+			toolbar: 'document'
+		});
+		
+    }
+});
